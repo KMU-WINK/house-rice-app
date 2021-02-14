4ce0d5a970943dd4ed249cd68829d00954632829
sunwoopia <sunwoopia@kookmin.ac.kr>
2021-02-14 19:32:15 +0900

    Bottom Tap Bar Example

--- App.js
+++ App.js
@@ -1,13 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {BottomNavigation} from 'react-native-material-ui'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <BottomNavigation style={styles.bottomTabBar}>
+          <BottomNavigation.Action
+              key="home"
+              icon="home"
+              label="홈"
+          />
+          <BottomNavigation.Action
+              key="category"
+              icon="category"
+              label="카테고리"
+          />
+          <BottomNavigation.Action
+              key="chat"
+              icon="chat"
+              label="채팅"
+          />
+          <BottomNavigation.Action
+              key="settings"
+              icon="settings"
+              label="설정"
+          />
+        </BottomNavigation>
+      </View>
   );
 }
 
@@ -15,7 +35,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
   },
+  bottomTabBar: {
+    marginBottom:30,
+  }
 });
